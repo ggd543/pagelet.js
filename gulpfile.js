c90a172bea1af5816f82571feec164d6ecbffe80
switer <guankaishe@gmail.com>
2015-03-26 15:50:48 +0800

    use gulp + webpack build to umd lib

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp')
+var webpack = require('gulp-webpack')
+var uglify = require('gulp-uglifyjs')
+var meta = require('./package.json')
+var watch = require('gulp-watch')
+
+
+gulp.task('watch', function () {
+    watch('lib/*.js', function () {
+        gulp.start('default')
+    })
+});
+
+gulp.task('default', function() {
+    return gulp.src('index.js')
+        .pipe(webpack({
+            output: {
+                library: 'pagelet',
+                libraryTarget: 'umd',
+                filename: 'pagelet.js'
+            }
+        }))
+        .pipe(gulp.dest('dist/'))
+        .pipe(uglify('pagelet.min.js', {
+            mangle: true,
+            compress: true
+        }))
+        .pipe(gulp.dest('dist/'))
+});
